75f42fd96c922649ba67ae3f11685652bff12009
jungdadak <junghyun21110@gmail.com>
2025-05-20 21:50:20 +0900

    chore: 노트북에서 작업하고 싶은 푸시

--- fetcher/getRank.ts
+++ fetcher/getRank.ts
@@ -1,3 +1,5 @@
+import { BE_URL } from "@/fetcher/uploadImage";
+
 export interface RankItem {
   userId: string;
   name: string;
@@ -7,7 +9,11 @@ export interface RankItem {
 
 export async function getRank(): Promise<RankItem[]> {
   try {
-    const res = await fetch("/face/ranking");
+    const res = await fetch(`${BE_URL}/face/ranking`);
+    console.log(`${BE_URL}/face/ranking`);
+    const text = await res.text();
+    console.log("응답 내용:", text);
+
     if (!res.ok) {
       throw new Error("랭킹 데이터를 불러오지 못했습니다");
     }
