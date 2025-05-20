d335af3ff6444a19d4fcb443aa1933053c6724e5
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-05-20 19:19:52 +0900

    fix: 모바일에서 상단 마진 큰 문제 해결

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -30,7 +30,7 @@ export default function LoadingPage() {
         alt=""
         className="absolute inset-0 -z-10 mx-auto h-full w-full object-cover"
       />
-      <h1 className="mx-auto mt-[115px] h-[78px] w-[304px] text-center text-[28px] leading-[39.2px] font-bold tracking-[-0.56px]">
+      <h1 className="mx-auto mt-20 h-[78px] w-[304px] text-center text-[28px] leading-[39.2px] font-bold tracking-[-0.56px]">
         {nickname}님의 감정을 <br /> 분석해 볼까요?
       </h1>
       <section
